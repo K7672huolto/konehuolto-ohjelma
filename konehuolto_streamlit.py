9fc633bdab02c60b7f32492fab4765ec164f73c9
K7672huolto <matti.palojarvi@outlook.com>
2025-07-19 06:40:28 +0300

    Update konehuolto_streamlit.py

--- konehuolto_streamlit.py
+++ konehuolto_streamlit.py
@@ -53,7 +53,7 @@ st.markdown(
         background-image: url('{kuva_base64}');
         background-size: cover;
         background-position: center;
-        padding: 70px 0 70px 0;
+        padding: 80px 0 80px 0;
         margin-bottom: 0.2em;
         text-align: center;
         width: 100vw;
