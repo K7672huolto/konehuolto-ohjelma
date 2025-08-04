f12727b32f8d72e1c24d6d72d359aebb31a9a0e0
K7672huolto <matti.palojarvi@outlook.com>
2025-08-04 16:15:47 +0300

    Update konehuolto_streamlit.py

--- konehuolto_streamlit.py
+++ konehuolto_streamlit.py
@@ -595,7 +595,8 @@ with tab4:
                     ])
                 st.success("Kaikkien koneiden tunnit tallennettu Google Sheetiin!")
             except Exception as e:
-                st.error(f"Tallennus epäonnistui: {e}"
+                st.error(f"Tallennus epäonnistui: {e}")
+
 
 
 
